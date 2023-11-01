eb5adcf01a83f8f3044699ebfb0d75259d92e3f5
t-uno-zg <146808664+t-uno-zg@users.noreply.github.com>
2023-11-01 10:04:46 +0900

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! GitHubAction deploy!!')
+        'body': json.dumps('Hello World 2023/11/01')
     }
